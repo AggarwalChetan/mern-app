89bc9ef33e9e03eed01cf7b056c3df62cc17b45a
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-19 11:35:06 +0530

    stateChange button added

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,6 +25,7 @@ class Login extends React.Component {
     }else{
       this.setState({type : 'Already have an account ?', signUprequest: 'Sign In'}); 
     }
+    this.setState({email : '', password : ''});
   }
 
   // login = () => {
